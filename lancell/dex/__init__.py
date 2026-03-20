cda1d0d2d909f9bc2ba74ee1b82c064f8cd870ca
conradry <conradry71@gmail.com>
2026-03-20 18:17:49 +0000

    updated numba to a default dependency, god help us

--- lancell/dex/__init__.py
+++ lancell/dex/__init__.py
@@ -1,14 +1,4 @@
-"""Differential expression analysis for lancell atlases.
-
-Requires the ``numba`` optional dependency::
-
-    pip install 'lancell[dex]'
-"""
-
-try:
-    import numba  # noqa: F401
-except ImportError as e:
-    raise ImportError("lancell.dex requires numba. Install with: pip install 'lancell[dex]'") from e
+"""Differential expression analysis for lancell atlases."""
 
 from lancell.dex._dex import dex
 
